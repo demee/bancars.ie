f13b12da44941580fb47dfffee70f5ece2a6d293
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2020-12-28 21:16:58 +0000

    Fixed tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElements = screen.getAllByAltText('g');
+  expect(linkElements.length).toBe(765)
 });
