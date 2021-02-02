4bd4e44a407602e0e2e3d98b715ec307a741249d
demee <slawomir.demichowicz@gmail.com>
2021-02-02 16:36:22 +0000

    Updated count

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Chart from './components/chart/Chart'
 function App() {
   return (
     <div className="App">      
-        <Chart></Chart>
+        <h2>Cars registerd in Ireland</h2>
+        <h1>2,805,839</h1> 
+        <h2>(and growing)</h2>
     </div>
   );
 }
