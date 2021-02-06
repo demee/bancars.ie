4fa920c58a1e15715607cbaf884301e66fed4037
demee <slawomir.demichowicz@gmail.com>
2021-02-06 23:08:04 +0000

    Counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -6,18 +6,15 @@ import './Counter.css';
 export default class Counter extends React.Component {
     constructor(props) {
         super(props)
-        this.targetDate = DateTime.fromFormat("2021", "y");
-        this.targetCount = 3000000;
-        this.startDate = DateTime.fromFormat("2019", "y");
+        this.targetDate = DateTime.fromObject({year: 2021, month: 12, day: 31});
+        this.targetCount = 3100000;
+        this.startDate = DateTime.fromObject({year: 2019, month: 12, day: 31});
         this.startCount = 2805839;
         this.state = {
             count: 0
         };
         this.style = "font-family: 'Roboto';";
-        setInterval(() => {
-            console.log('recalculating')
-            this.recalculate() 
-        }, 1000)
+        this.recalculate()
     }
     recalculate() {
         let int = Interval.fromDateTimes(this.startDate, this.targetDate).count('seconds');
@@ -29,12 +26,16 @@ export default class Counter extends React.Component {
         this.setState({
             count: this.startCount + Math.round((todayInt/int) * diff)
         });
+        setTimeout(() => {
+            console.log('recalculating')
+            this.recalculate() 
+        }, 1000)
     }
     
     render() {
         return (<div>
             <h2>There are</h2>
-            <svg viewBox="0 0 60 18">
+            <svg viewBox="0 0 63  18">
                 <defs>
                     <style type="text/css">
                         @import url('https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic');
