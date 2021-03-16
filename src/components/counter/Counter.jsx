6f7063c51a968426f9ec4e3e1a4077372061a590
demee <slawomir.demichowicz@gmail.com>
2021-03-16 16:39:31 +0000

    Added star

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -5,62 +5,66 @@ import NumberFormat from 'react-number-format';
 import './Counter.css';
 
 export default class Counter extends React.Component {
-    constructor(props) {
-        super(props)    
-        this.state = {
-            barWidth: 0
-        };
-        this.style = "font-family: 'Roboto';";   
-         
-    }
-    tick() {
-        console.log(".")
+  constructor(props) {
+    super(props)
+    this.state = {
+      barWidth: 0
+    };
+    this.style = "font-family: 'Roboto';";
 
-        let int = Interval.fromDateTimes(this.props.startDate, this.props.targetDate).count('seconds');
+  }
+  tick() {
+    console.log(".")
 
-        if (int.invalid) {
-          throw new Error(int.invalid);
-        }
+    let int = Interval.fromDateTimes(this.props.startDate, this.props.targetDate).count('seconds');
 
-        let diff = this.props.targetCount - this.props.startCount;
-
-        let todayInt = Interval.fromDateTimes(this.props.startDate, DateTime.local()).count('seconds');
-
-        this.setState({
-            barWidth: ((todayInt/int * diff) % 1) * 100,
-            count: this.props.startCount + Math.floor((todayInt / int) * diff)
-        });
+    if (int.invalid) {
+      throw new Error(int.invalid);
     }
 
-    componentDidMount() {
-        this.tick();
-        this.intervalID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
+    let diff = this.props.targetCount - this.props.startCount;
 
-    componentWillUnmount() {
-        clearInterval(this.intervalID);
-    }
+    let todayInt = Interval.fromDateTimes(this.props.startDate, DateTime.local()).count('seconds');
 
-    render() {
-        return (<div>
-            <h2>There are</h2>
-            <svg viewBox="0 0 100 25">
-                <defs>
-                    <style type="text/css">
-                        @import url('https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic');
-                    </style>
-                </defs>
-                <text x="5" y="17" style={{ fontFamily: 'Yusei Magic', fontSize: '20px'}} >
-                    <NumberFormat value={this.state.count}  displayType={'text'} thousandSeparator={true} renderText={value => value}/>
-                </text>
-                <rect x={100 - this.state.barWidth} y="20" width={this.state.barWidth} height="2"></rect>
-                <text x={100 - this.state.barWidth - 2} y="22" style={{fontSize: '3px'}}>🚗</text>
-                <text x={100 - this.state.barWidth + 2} y="21.5" style={{fontSize: '2px', fill: '#fff'}}>a new one is coming... brum brum</text>
-            </svg>
-            <h2>cars too many on irish roads, #bancars</h2>         
-        </div>)
-    }
+    this.setState({
+      barWidth: ((todayInt / int * diff) % 1) * 100,
+      count: this.props.startCount + Math.floor((todayInt / int) * diff)
+    });
+  }
+
+  componentDidMount() {
+    this.tick();
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  render() {
+    return (<div>
+      <h2>There are</h2>
+      <svg viewBox="0 0 100 25">
+        <defs>
+          <style type="text/css">
+            @import url('https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic');
+          </style>
+        </defs>
+        <text x="5" y="17" style={{ fontFamily: 'Yusei Magic', fontSize: '20px' }} >
+          <NumberFormat value={this.state.count} displayType={'text'} thousandSeparator={true} renderText={value => value} />
+        </text>
+        <text x="93" y="10" style={{ cursor: "pointer", fill: "blue", fontFamily: 'Yusei Magic', fontSize: '10px', fontStyle: 'underscore'}}>
+          *
+        </text>
+        <rect x={100 - this.state.barWidth} y="20" width={this.state.barWidth} height="2"></rect>
+        <text x={100 - this.state.barWidth - 2} y="22" style={{ fontSize: '3px' }}>🚗</text>
+        <text x={100 - this.state.barWidth + 2} y="21.5" style={{ fontSize: '2px', fill: '#fff' }}>a new one is coming... brum brum</text>
+      </svg>
+      <h2>cars too many on irish roads, #bancars</h2>
+      <span style={{float: 'left', marginLeft: '10px'}}>* - estimated</span>
+    </div>)
+  }
 }
