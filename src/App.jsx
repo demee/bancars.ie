ae6a89ec95856d1067d60fcd145c5a7260556aef
demee <slawomir.demichowicz@gmail.com>
2021-04-18 19:34:47 +0000

    Added propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import './App.css';
 import React from 'react';
-import Counter from './components/counter/Counter';
 import { DateTime } from 'luxon';
+import './App.css';
+import Counter from './components/counter/Counter';
 
 function App() {
-  let carCountStart = 2805839;
-  let carCountTarget = 2805839 + 88000 + 100000; 
-  let carCountDateStart = DateTime.fromObject({ year: 2019, month: 12, day: 31 });
-  let carCountDateEnd = DateTime.fromObject({ year: 2021, month: 12, day: 31 });
+  const carCountStart = 2805839;
+  const carCountTarget = 2805839 + 88000 + 100000;
+  const carCountDateStart = DateTime.fromObject({ year: 2019, month: 12, day: 31 });
+  const carCountDateEnd = DateTime.fromObject({ year: 2021, month: 12, day: 31 });
   return (
-    <div className="App">      
-      <Counter 
+    <div className="App">
+      <Counter
         startDate={carCountDateStart}
         targetDate={carCountDateEnd}
-        startCount={carCountStart} 
-        targetCount={carCountTarget}>
-      </Counter>
+        startCount={carCountStart}
+        targetCount={carCountTarget}
+      />
     </div>
   );
 }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DateTime, Interval } from 'luxon';
+import PropTypes from 'prop-types';
 import NumberFormat from 'react-number-format';
 
 import './Counter.css';
@@ -26,7 +27,9 @@ export default class Counter extends React.Component {
   }
 
   tick() {
-    const {startDate, targetDate} = this.props;
+    const {
+      startDate, targetDate, targetCount, startCount,
+    } = this.props;
 
     const int = Interval.fromDateTimes(startDate, targetDate).count('seconds');
 
@@ -34,28 +37,29 @@ export default class Counter extends React.Component {
       throw new Error(int.invalid);
     }
 
-    const diff = this.props.targetCount - this.props.startCount;
+    const diff = targetCount - startCount;
 
-    const todayInt = Interval.fromDateTimes(this.props.startDate, DateTime.local()).count('seconds');
+    const todayInt = Interval.fromDateTimes(startDate, DateTime.local()).count('seconds');
 
     this.setState({
-      barWidth: ((todayInt / int * diff) % 1) * 100,
-      count: this.props.startCount + Math.floor((todayInt / int) * diff),
+      barWidth: (((todayInt / int) * diff) % 1) * 100,
+      count: startCount + Math.floor((todayInt / int) * diff),
     });
   }
 
   render() {
+    const { count, barWidth } = this.state;
     return (
       <div>
         <h2>There are</h2>
         <svg viewBox="0 0 100 25">
           <defs>
             <style type="text/css">
-              @import url('https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic');
+              @import url(https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic);
             </style>
           </defs>
           <text x="5" y="17" style={{ fontFamily: 'Yusei Magic', fontSize: '20px' }}>
-            <NumberFormat value={this.state.count} displayType="text" thousandSeparator renderText={(value) => value} />
+            <NumberFormat value={count} displayType="text" thousandSeparator renderText={(value) => value} />
           </text>
           <text
             x="93"
@@ -66,9 +70,9 @@ export default class Counter extends React.Component {
           >
             *
           </text>
-          <rect x={100 - this.state.barWidth} y="20" width={this.state.barWidth} height="2" />
-          <text x={100 - this.state.barWidth - 2} y="22" style={{ fontSize: '3px' }}>🚗</text>
-          <text x={100 - this.state.barWidth + 2} y="21.5" style={{ fontSize: '2px', fill: '#fff' }}>a new one is coming... brum brum</text>
+          <rect x={100 - barWidth} y="20" width={barWidth} height="2" />
+          <text x={100 - barWidth - 2} y="22" style={{ fontSize: '3px' }}>🚗</text>
+          <text x={100 - barWidth + 2} y="21.5" style={{ fontSize: '2px', fill: '#fff' }}>a new one is coming... brum brum</text>
         </svg>
         <h2>cars too many on irish roads, #bancars</h2>
         <span style={{ float: 'left', marginLeft: '10px' }}>* - estimated</span>
@@ -76,3 +80,14 @@ export default class Counter extends React.Component {
     );
   }
 }
+
+Counter.propTypes = {
+  startDate: PropTypes.string.isRequired,
+  targetDate: PropTypes.string.isRequired,
+  targetCount: PropTypes.number.isRequired,
+  startCount: PropTypes.number,
+};
+
+Counter.defaultProps = {
+  startCount: 0,
+};
