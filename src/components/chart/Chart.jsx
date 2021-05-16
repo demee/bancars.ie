b2547ef3462fa4234f351d5554fec54499e9220d
demee <slawomir.demichowicz@gmail.com>
2021-05-16 16:45:34 +0000

    Eslint

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 
 export default class Chart extends React.Component {
-    constructor(props) {
-        super(props);
-        this.margin = {
-            top: 10, 
-            bottom: 10, 
-            left: 10, 
-            right: 10
-        }
-        this.width = 1920;
-        this.height = 1080
-        this.state = {
-            viewBox: `0,0,${this.width},${this.height}`,
-            translateXAxis: `translate(0,${this.height - this.margin.bottom})`
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.margin = {
+      top: 10,
+      bottom: 10,
+      left: 10,
+      right: 10,
+    };
+    this.width = 1920;
+    this.height = 1080;
+    this.state = {
+      viewBox: `0,0,${this.width},${this.height}`,
+      translateXAxis: `translate(0,${this.height - this.margin.bottom})`,
+    };
+  }
 
-    renderXAxis() {
-        return <g transform={this.state.translateXAxis}></g>
-    }
+  renderXAxis() {
+    return <g transform={this.state.translateXAxis} />;
+  }
 
-    renderYAxis() {
-        return <text>Yaxis</text>
-    }
+  renderYAxis() {
+    return <text>Yaxis</text>;
+  }
 
-    render() {
-        const XAxis = this.renderXAxis();
-        const Yaxis = this.renderYAxis();
+  render() {
+    const XAxis = this.renderXAxis();
+    const Yaxis = this.renderYAxis();
 
-        return (<svg viewBox={this.state.viewBox}>
-                {XAxis}{Yaxis}
-        </svg>);
-    }
+    return (
+      <svg viewBox={this.state.viewBox}>
+        {XAxis}
+        {Yaxis}
+      </svg>
+    );
+  }
 }
--- src/components/chart/Chart.test.jsx
+++ src/components/chart/Chart.test.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { act } from "react-dom/test-utils";
+import { act } from 'react-dom/test-utils';
 import Chart from './Chart';
 
-
 test('renders Chart', () => {
-    render(<Chart />);
-    screen.findAllByText('Banana').then((elements) => {
-        expect(elements.length).toEqual(1)
-    })
+  render(<Chart />);
+  screen.findAllByText('Banana').then((elements) => {
+    expect(elements.length).toEqual(1);
+  });
 });
