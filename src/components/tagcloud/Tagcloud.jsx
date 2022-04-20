7523de906e3e4578c3e0d151854b5d3b230f426b
demee <slawomir.demichowicz@gmail.com>
2022-04-20 21:55:42 +0000

    Fixed linting problems

--- src/components/tagcloud/Tagcloud.jsx
+++ src/components/tagcloud/Tagcloud.jsx
@@ -1,24 +1,23 @@
+import React from 'react';
 import d3cloud from 'd3-cloud';
 
 export default function Tagcloud() {
-    const words = [{
-        text: "Cars", 
-        size: 100
-    }, 
-    {
-        text: "Pollution",
-        size: 30
-    }];
-    const layout = d3cloud()
-        .size([500, 500])
-        .words(words)
-    debugger;
-    console.log(layout);
+  const words = [{
+    text: 'Cars',
+    size: 100,
+  },
+  {
+    text: 'Pollution',
+    size: 30,
+  }];
+  const layout = d3cloud()
+    .size([500, 500])
+    .words(words);
+  console.log(layout);
 
-    return (<svg>
-        {layout.words().map(w => {
-            console.log(w)
-            return <text key={w.text}>w.text</text>;
-        })}
-    </svg>);
+  return (
+    <svg>
+      {layout.words().map((w) => <text key={w.text}>w.text</text>)}
+    </svg>
+  );
 }
