bf8f4fe0aafe66c6567a3d512250e418642cb337
demee <slawomir.demichowicz@gmail.com>
2022-05-21 22:13:17 +0000

    Articles

--- src/pages/TagBody/TagBody.jsx
+++ src/pages/TagBody/TagBody.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function TagBody() {
   const params = useParams();
+  const [articles, setArticles] = useState([]);
+  useEffect(async () => {
+    const articlesResponse = await fetch('/data/articles.json');
+    const articleData = await articlesResponse.json();
+    setArticles(articleData.filter((a) => a.tags.includes(params.tag.toLowerCase())));
+  }, []);
 
   return (
     <div>
-      Articles:
-      {' '}
-      { params.tag }
+      {articles.map((a) => <a href={a.url} key={a.url}>{a.title}</a>)}
     </div>
   );
 }
