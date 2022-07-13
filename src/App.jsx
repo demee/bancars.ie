679f4c8aeb5be3d7ca2498232edfeeac23fac88e
demee <slawomir.demichowicz@gmail.com>
2022-07-13 20:24:17 +0000

    Trying to make fonts work

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { CssBaseline, GlobalStyles } from '@mui/material';
+import { Box, CssBaseline, GlobalStyles } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-import './App.css';
 import Home from './pages/home/Home';
 import TagBody from './pages/TagBody/TagBody';
 import defaultTheme from './themes/Default';
@@ -18,12 +17,17 @@ function App() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <GlobalStyles styles={globalSyle} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tags/:tag" element={<TagBody />} />
-        </Routes>
-      </BrowserRouter>
+      <Box sx={{
+        fontFamily: 'Montserrat Alternates',
+      }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tags/:tag" element={<TagBody />} />
+          </Routes>
+        </BrowserRouter>
+      </Box>
     </ThemeProvider>
   );
 }
