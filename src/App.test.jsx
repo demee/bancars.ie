0079403a30325ffa4a9a8cd2b57914045d155ab4
demee <slawomir.demichowicz@gmail.com>
2022-08-25 22:20:56 +0000

    Updated test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
 describe('app', () => {
   it('renders learn react link', () => {
-    expect.assertions(1);    
+    expect.assertions(1);
     expect(true).toBe(true);
   });
 });
